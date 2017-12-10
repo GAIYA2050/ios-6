58c894f0c17db5461fe975bfba1ea039f7a8f0bf
jyz <jyz@jyzdeiMac.local>
2017-12-10 21:25:41 +0800

    sdfc

--- 111/111/ViewController.swift
+++ 111/111/ViewController.swift
@@ -13,8 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
+        demo()
+        demo1()
     }
+    func demo1(){
+        let str = "hello world"
+        print(str.lengthOfBytes(using: .utf8))
+        
+        print(str.characters.count)//字符串长度
+        
+        let ocStr = str as NSString//as
+        print(ocStr.length)
+    }
+    
+    func demo(){
+        let str = "我要飞的更高"//nNSSTRING 不支持遍历
+        for c in str.characters{
+            print(c)
+        }
 
     
 }
-
+}
